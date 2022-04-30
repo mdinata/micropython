5dc1de3d8aecb56a37bf21f788584e7841b15726
mdinata <34206130+mdinata@users.noreply.github.com>
2022-04-30 07:50:39 +0700

    Update and rename form_demo.py to form_demo_oled.py

--- gui/form_demo_oled.py
+++ gui/form_demo_oled.py
@@ -1,10 +1,13 @@
+#oled display initiation part
 from machine import Pin, I2C
 import ssd1306
 import time
-from forms import *
 
 i2c=I2C(scl=Pin(5), sda=Pin(4), freq=100000)
 display=ssd1306.SSD1306_I2C(128,64,i2c)
+
+#form library part
+from forms import *
 
 f1=Form(display,"Volume",0,0,20,10,invert=False,fill=True)
 f2=Form(display,"Total ",0,11,20,10,invert=True,fill=True)
