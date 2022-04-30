b630e07882126df2025b1956977e84689b9a6328
mdinata <34206130+mdinata@users.noreply.github.com>
2022-04-30 07:39:39 +0700

    Update form_demo.py

--- gui/form_demo.py
+++ gui/form_demo.py
@@ -6,10 +6,10 @@ from forms import *
 i2c=I2C(scl=Pin(5), sda=Pin(4), freq=100000)
 display=ssd1306.SSD1306_I2C(128,64,i2c)
 
-f1=Form("Volume",0,0,20,10,invert=False,fill=True)
-f2=Form("Total ",0,11,20,10,invert=True,fill=True)
-f3=Form("/liter",0,22,20,10,invert=False,fill=True)
+f1=Form(display,"Volume",0,0,20,10,invert=False,fill=True)
+f2=Form(display,"Total ",0,11,20,10,invert=True,fill=True)
+f3=Form(display,"/liter",0,22,20,10,invert=False,fill=True)
 
-t1=Textbox("Pertamax",0,34)
+t1=Textbox(display,"Pertamax",0,34)
 
 display.show()
